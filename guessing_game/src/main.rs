44580793b0b75f6754e5107d0d1e16635d582d5c
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-07-13 11:37:49 +0530

    Completed guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,38 @@
+use std::io; //Standard library for taking input from user
+use std::cmp::Ordering;
+use rand::Rng;
+
+
 fn main() {
-    println!("Hello, world! trying to commit");
+    println!("Guessing Game");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    loop{
+        println!("Enter a number");
+        let mut input_guess = String::new();
+        io::stdin()
+            .read_line(&mut input_guess)
+            .expect("Failed to read input");
+
+        println!("You guessed {}",input_guess);
+
+        //This step is to convert the string input into an integer
+        let input_guess : i32 = match input_guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Enter a number");
+                continue;
+            } 
+        };
+
+        match input_guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too less"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            },
+            Ordering::Greater => println!("Too big")
+        };
+    }
+    
 }
