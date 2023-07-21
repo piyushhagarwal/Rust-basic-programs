7eb8158fa23387b95654556a59af96d215c4dca1
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-07-21 11:59:02 +0530

    basic api which returns a json

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,11 +1,12 @@
-use axum::{Router, routing::get, response::Html};
+use axum::{Router, routing::get, Json};
 use std::net::SocketAddr;
+use serde::Serialize;
 
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-    .route("/hello",get(handler_function));
+        .route("/hello",get(handler_function));
 
 
     //Code to start the server
@@ -16,8 +17,18 @@ async fn main() {
         .await
         .unwrap();
 
-    async fn handler_function () -> Html<&'static str>{
-        Html("<h1>Hello world</h1>")
+
+    #[derive(Serialize)]
+    struct Message {
+        message: String,
+    }
+
+    //This function returns a json object as a response for this function.
+    async fn handler_function () -> Json<Message>{
+        Json(Message { 
+            message: String::from("Hello, world") 
+        })
+        //This Json function serializes(convert struct into json) the Message struct.
     }
 }
 
