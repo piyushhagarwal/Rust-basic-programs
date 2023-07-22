e9acaf3fd34e15981185c252d116e75d4bd38751
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-07-22 13:45:31 +0530

    Basic operations on query parameters, dynamic routes and post req

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -3,19 +3,24 @@
 //command to run the server: "cargo watch -x run"
 
 
-use axum::{Router, routing::get, Json};
+use axum::{Router, routing::{get,post}, Json};
+use axum::extract::{Path, Query};
 use std::net::SocketAddr;
-use serde::Serialize;
+use serde::{Serialize,Deserialize};
 
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/hello",get(handler_function));
+        .route("/hello",get(handler_function))
+        .route("/test/:id", get(handle_id)) //syntax for handling dynamic routes
+        .route("/:user_id/team/:team_id",get(handle_multiple_ids)) //Syntax for handling 2 or more dynamic parameters
+        .route("/queryparams", get(handle_query))
+        .route("/post", post(handle_post));
 
 
     //Code to start the server
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -34,6 +39,38 @@ async fn main() {
             message: String::from("Hello, world. I am test") 
         })
         //This Json function serializes(convert struct into json) the Message struct.
+    }
+
+    //This function returns a string which holds the value of dynamic id.
+    async fn handle_id(Path(id): Path<String>) -> String {
+        format!("{id}")
+    }
+
+    //This function returns a string which holds the value of dynamic user_id. and team_id
+    async fn handle_multiple_ids(Path((user_id,team_id)): Path<(u32,String)>) -> String {
+        format!("user_id: {}, team_id: {}",user_id,team_id )
+    }
+
+    #[derive(Serialize,Deserialize)]
+    struct QueryParams{
+        message : String,
+        id : i32
+    }
+
+    //This function returns a json of QueryParams type
+    async fn handle_query(Query(query): Query<QueryParams>) -> Json<QueryParams>{
+        Json(query)
+    }
+
+    #[derive(Serialize,Deserialize)]
+    struct UserData {
+        name: String,
+        age: u32,
+    }
+
+    //This function gets the body of the api as an argument and returns a json of UserData type
+    async fn handle_post(user_data: Json<UserData>) -> Json<UserData> {
+        Json(UserData { name: user_data.name.to_string(), age: user_data.age })
     }
 }
 
