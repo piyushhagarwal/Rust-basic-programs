b909765a38a1b725bb24542b642005dd1e2f7b57
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-07-28 11:36:13 +0530

    built a simple get req

--- reqwest/src/main.rs
+++ reqwest/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let result = reqwest::get("https://api.spotify.com/v1/search").await.expect("Error in fetching the api");
+    //This is a simple get request
+
+    println!("{:#?}",result);//This syntax helps to prettify the output
 }
