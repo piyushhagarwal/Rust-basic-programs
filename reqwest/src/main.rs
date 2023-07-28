e9de8e2798f142245fa76248556c10c5401962b6
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-07-28 12:45:25 +0530

    Added client method

--- reqwest/src/main.rs
+++ reqwest/src/main.rs
@@ -1,7 +1,27 @@
 #[tokio::main]
 async fn main() {
-    let result = reqwest::get("https://api.spotify.com/v1/search").await.expect("Error in fetching the api");
+
     //This is a simple get request
+    let result = reqwest::get("https://api.spotify.com/v1/search").await.unwrap().text().await.unwrap();
+    //.text gives the actual body of the response
 
     println!("{:#?}",result);//This syntax helps to prettify the output
+
+    //Creating a get request using Client and adding headers to it
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("https://api.spotify.com/v1/search")
+        .header("AUTHORIZATION", "Bearer [AUTH_TOKEN]")
+        .header("CONTENT_TYPE", "application/json")
+        .header("ACCEPT", "application/json")
+        .send()
+        .await.
+        unwrap().
+        text().
+        await;
+
+    println!("{:#?}",response)
+
 }
