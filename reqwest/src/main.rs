373c98067353c17c6c123b000f387fd003b0a1d4
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-07-29 13:42:08 +0530

    Created an api for fetching data from news api

--- reqwest/src/main.rs
+++ reqwest/src/main.rs
@@ -1,15 +1,27 @@
+use serde::Deserialize;
 
-struct Source<T>{
-    id: Option<T>,
+
+#[derive(Debug,Deserialize)]
+struct Source{
+    id: Option<String>,
     name: String,
 }
+#[derive(Debug,Deserialize)]
 struct Article{
-    source: Source<i32>,
-    name: String,
-    author: String,
+    source: Source,
+    author: Option<String>,
     title: String,
-    description: String
-
+    description: String,
+    url : String,
+    urlToImage: Option<String>,
+    publishedAt: String,
+    content: String
+}
+#[derive(Debug, Deserialize)]
+struct Response{
+    status: String,
+    totalResults: u32,
+    articles: Vec<Article>
 }
 
 
@@ -19,11 +31,12 @@ async fn main() {
     let api_key = "7d20478f4cc6481eb401e66d7d5ca7b6";
     //Creating a get request using Client and adding headers to it
   
-    let url = format!("https://newsapi.org/v2/everything?q=tesla&from=2023-06-28&sortBy=publishedAt&apiKey={api_key}");
+    let url = format!("https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey={}", api_key);
+
 
     let client = reqwest::Client::new();
 
-    let response = client
+    let json_str = client
         .get(url)
         .send()
         .await.
@@ -31,6 +44,9 @@ async fn main() {
         .text()
         .await
         .unwrap();
+
+    let response : Response = serde_json::from_str(&json_str).expect("Failed to deserialize JSON");
+
 
     println!("{:#?}",response)
 
